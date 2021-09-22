8f349b8b4c5dfbee788e0bf3428d36e41e330668
YUTA UEHARA <mame5124@gmail.com>
2021-09-23 05:03:21 +0900

    Revert "## 記事作成の実装"
    This reverts commit 1228cf7b5f883f1adb290213209eff15fb0c6672.

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,29 +1,13 @@
 module Api::V1
   class ArticlesController < BaseApiController
     def index
-      articles = Article.all
-      render json: articles
+      articles = Article.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
     end
 
     def show
       article = Article.find(params[:id])
-      render json: article
+      render json: article, serializer: Api::V1::ArticleSerializer
     end
-
-    def create
-      article = current_user.articles.create!(article_params)
-      render json: article
-    end
-
-    def update
-      article = current_user.articles.find(params[:id])
-      article.update!(article_params)
-    end
-
-    private
-
-      def article_params
-        params.require(:article).permit(:title, :body)
-      end
   end
 end
--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,5 +1,2 @@
 class Api::V1::BaseApiController < ApplicationController
-  def current_user
-    @current_user ||= User.first
-  end
 end
