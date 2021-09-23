e0a97f90f43b249fa7ea1fca800b2186d9533066
YUTA UEHARA <mame5124@gmail.com>
2021-09-24 00:59:21 +0900

    ## 記事更新の実装 - 記事更新に関する API と テストを実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -18,6 +18,7 @@ module Api::V1
     def update
       article = current_user.articles.find(params[:id])
       article.update!(article_params)
+      render json: article, serializer: Api::V1::ArticleSerializer
     end
 
     private
--- spec/requests/api/v1/articles_request_spec.rb
+++ spec/requests/api/v1/articles_request_spec.rb
@@ -1,23 +1,41 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
-  describe "GET /articles/:id" do
+  describe "GET /articles" do
+    subject { get(api_v1_articles_path) }
+
+    before { create_list(:article, 3) }
+
+    it "記事の一覧が取得できる" do
+      subject
+
+      res = JSON.parse(response.body)
+
+      aggregate_failures do
+        expect(response).to have_http_status(:ok)
+        expect(res.length).to eq 3
+        expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "user"]
+        expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+      end
+    end
+  end
+
+  describe "GET /users/:id" do
     subject { get(api_v1_article_path(article_id)) }
 
     context "指定した id の記事が存在する場合" do
       let(:article) { create(:article) }
       let(:article_id) { article.id }
 
-      it "任意の記事の値が取得できる" do
-        aggregate_failures do
-          subject
-          res = JSON.parse(response.body)
+      it "任意の記事が取得できる" do
+        subject
 
+        res = JSON.parse(response.body)
+        aggregate_failures do
           expect(response).to have_http_status(:ok)
           expect(res["id"]).to eq article.id
           expect(res["title"]).to eq article.title
           expect(res["body"]).to eq article.body
-          expect(res["updated_at"]).to be_present
           expect(res["user"]["id"]).to eq article.user.id
           expect(res["user"].keys).to eq ["id", "name", "email"]
         end
@@ -48,6 +66,34 @@ RSpec.describe "Api::V1::Articles", type: :request do
         expect(res["title"]).to eq params[:article][:title]
         expect(res["body"]).to eq params[:article][:body]
         expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "PATCH /api/v1/articles/:id" do
+    subject { patch(api_v1_article_path(article.id), params: params) }
+
+    let(:params) { { article: attributes_for(:article) } }
+    let(:current_user) { create(:user) }
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    context "自分が所持している記事のレコードを更新しようとするとき" do
+      let(:article) { create(:article, user: current_user) }
+
+      it "記事を更新できる" do
+        expect { subject }.to change { article.reload.title }.from(article.title).to(params[:article][:title]) &
+                              change { article.reload.body }.from(article.body).to(params[:article][:body])
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "自分が所持していない記事のレコードを更新しようとするとき" do
+      let(:other_user) { create(:user) }
+      let!(:article) { create(:article, user: other_user) }
+
+      it "更新できない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound) &
+                              change { Article.count }.by(0)
       end
     end
   end
